15ff73270369786008fb1ba33a8d5aa5f110a834
micho <gatin13@yahoo.com>
2016-08-22 14:56:31 +0900

    Completed the assignment

--- week 02/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ week 02/quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -6,6 +6,7 @@ import org.scalacheck._
 import Arbitrary._
 import Gen._
 import Prop._
+import Math.min
 
 abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
@@ -16,28 +17,59 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
 
   implicit lazy val arbHeap: Arbitrary[H] = Arbitrary(genHeap)
 
-  property("gen1") = forAll { h: H =>
-    val m = if (isEmpty(h)) 0 else findMin(h)
-    findMin(insert(m, h)) == m
-  }
+  property("gen1") =
+    forAll { h: H =>
+      val m = if (isEmpty(h)) 0 else findMin(h)
+      findMin(insert(m, h)) == m
+    }
 
-  property("minimum of 1") = forAll { a: Int =>
-    findMin(insert(a, empty)) == a
-  }
+  property("the minimum of a heap with 1 element is said element") =
+    forAll { a: Int =>
+      findMin(insert(a, empty)) == a
+    }
 
-  property("minimum of 2") = forAll { (a: Int, b: Int) =>
-    val h = insert(b, insert(a, empty))
-    val e1 = findMin(h)
-    val e2 = findMin(deleteMin(h))
-    e1 <= e2
-  }
+  property("insert any 2 elements into an empty heap. Finding the minimum shall return the smallest of the 2 elements") =
+    forAll { (a: Int, b: Int) =>
+      val h = insert(b, insert(a, empty))
+      findMin(h) == min(a, b)
+    }
 
-  property("delete last element in the heap should produce an empty heap") = forAll { a: Int =>
-    deleteMin(insert(a, empty)) == empty
-  }
+  property("insert an element into an empty heap and delete one element from it should produce an empty heap") =
+    forAll { a: Int =>
+      isEmpty(deleteMin(insert(a, empty)))
+    }
 
-//  property("continually finding and deleting the minimum produces a sorted sequence") = ???
+  property("continually finding and deleting the minimum should produce a sorted sequence") =
+    forAll {
+      h: H =>
+        def isSorted(h: H): Boolean =
+          if (isEmpty(h)) true
+          else {
+            val minimum = findMin(h)
+            val h2 = deleteMin(h)
+            isEmpty(h2) || (minimum <= findMin(h2) && isSorted(h2))
+          }
+        isSorted(h)
+    }
 
-//  property("the minimum of the meld of 2 heaps should be the minimum of 1 of them") = ???
+  property("the minimum of the meld of 2 heaps should be the minimum of both heaps") =
+    forAll {
+      (h1: H, h2: H) =>
+        findMin(meld(h1, h2)) == min(findMin(h1), findMin(h2))
+    }
+
+  property("2 heaps should be equal if continually removing minimum elements results in equal heaps") =
+    forAll {
+      (h1: H, h2: H) =>
+        def heapEqual(h1: H, h2: H): Boolean =
+          if (isEmpty(h1) && isEmpty(h2)) true
+          else {
+            val minimum1 = findMin(h1)
+            val minimum2 = findMin(h2)
+            (minimum1 == minimum2) && heapEqual(deleteMin(h1), deleteMin(h2))
+          }
+        heapEqual(meld(h1, h2), meld(deleteMin(h1), insert(findMin(h1), h2)))
+
+    }
 
 }
